7d8803cdaf53b1b087367c71318056750ee41102
WilliamLebeuf <115445658+WilliamLebeuf@users.noreply.github.com>
2023-06-19 10:36:54 -0400

    Update program.java

--- WLebeuf/program.java
+++ WLebeuf/program.java
@@ -1,10 +1,10 @@
-class program
+Iclass program
 {
 
 public static void main(String args[])
     {
         System.out.print("water is blue, i like to moo
         grass is green, i am at a canteen 
-        mom's spaguetti")
+        mom's spaguetti EDITED")
     }
 
